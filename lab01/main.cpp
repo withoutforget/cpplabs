fbec459e251f15d83373f171178e2324af0574b9
Roman Kovach <idcfyoy@gmail.com>
2026-02-19 10:48:00 +0300

    some fixes

--- lab01/main.cpp
+++ lab01/main.cpp
@@ -1,4 +1,5 @@
 #include <functional>
+#include <iomanip>
 #include <iostream>
 #include <stdexcept>
 
@@ -8,10 +9,12 @@ constexpr double g_derivative_eps = 0.00001;
 constexpr double g_newton_method_eps = 0.001;
 constexpr size_t g_newton_method_max_iter = 1'000;
 constexpr size_t g_integration_steps = 10'000;
+constexpr size_t g_output_precision = 10;
 
 math_f derivative(math_f func) {
     return [func](double x) -> double {
-        return (func(x + g_derivative_eps) - func(x - g_derivative_eps)) /
+        return (func(x + g_derivative_eps) -
+                func(x - g_derivative_eps)) /
                (2 * g_derivative_eps);
     };
 }
@@ -37,9 +40,8 @@ double intersection(math_f f, math_f g, double x0 = .0) {
     auto h = [f, g](double x) -> double {
         return f(x) - g(x);
     };
-    return intersection(h);
+    return intersection(h, x0);
 }
-
 
 double integrate(math_f f, double a, double b) {
     double step = (b - a) / g_integration_steps;
@@ -51,19 +53,27 @@ double integrate(math_f f, double a, double b) {
     return sum * step;
 }
 
-double f(double x) {
-    return - x * x + 6.0 * x - 5.0;
-}
-
-double g(double x) {
-    return - x / 3.0 - 1.0 / 2.0;
-}
+double f(double x) { return x; }
+double g(double x) { return x * x - 2.0; }
 
 int main() {
-    double a = intersection(f);
-    double b = intersection(f, g);
+    double a = intersection(f, g);
+    double b = intersection(f, g, 10);
     if (b < a)
         std::swap(a, b);
-    auto s = integrate([](double x) -> double { return f(x) - g(x); }, a, b);
-    std::cout << s << std::endl;
+
+    std::cout << "a = " << std::fixed
+              << std::setprecision(g_output_precision) << a
+              << std::endl;
+    std::cout << "b = " << std::fixed
+              << std::setprecision(g_output_precision) << b
+              << std::endl;
+
+    auto s = integrate(
+        [](double x) -> double { return f(x) - g(x); }, a,
+        b);
+
+    std::cout << "I = " << std::fixed
+              << std::setprecision(g_output_precision) << s
+              << std::endl;
 }
